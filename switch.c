d26abc711a834949050386eba2cf15e424707890
KyleWyckoff <105688322+KyleWyckoff@users.noreply.github.com>
2025-04-10 12:41:24 -0700

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,30 @@
+// switches.c
+// Runs on TM4C123, starter file for space invader game
+// Min He
+// November 15, 2022
+
+#include "tm4c123gh6pm.h"
+#include <stdint.h>
+
+// Initialize the onboard two switches.
+void Switch_Init(void){// Initilize port F and arm PF4, PF0 for falling edge interrupts
+	unsigned long volatile delay;
+  SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOF; // (a) activate clock for port F
+  delay = SYSCTL_RCGC2_R;
+  GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY; // unlock GPIO Port F
+  GPIO_PORTF_CR_R |= 0x1F;         // allow changes to PF4,0
+  GPIO_PORTF_DIR_R &= ~0x11;    // (c) make PF4,0 in (built-in button)
+  GPIO_PORTF_DIR_R |= 0x0E;     // make PF1-3 out
+  GPIO_PORTF_AFSEL_R &= ~0x11;  //     disable alt funct on PF4,0
+  GPIO_PORTF_DEN_R |= 0x1F;     //     enable digital I/O on PF4,0
+  GPIO_PORTF_PCTL_R &= ~0x000FFFFF; //  configure PF4,0 as GPIO
+  GPIO_PORTF_AMSEL_R &= ~0x1F;  //     disable analog functionality on PF4,0
+  GPIO_PORTF_PUR_R |= 0x11;     //     enable weak pull-up on PF4,0
+  GPIO_PORTF_IS_R &= ~0x11;     // (d) PF4,PF0 is edge-sensitive
+  GPIO_PORTF_IBE_R &= ~0x11;    //     PF4,PF0 is not both edges
+  GPIO_PORTF_IEV_R |= 0x11;    //     PF4,PF0 rising edge event
+  GPIO_PORTF_ICR_R = 0x11;      // (e) clear flags 4,0
+  GPIO_PORTF_IM_R |= 0x11;      // (f) arm interrupt on PF4,PF0
+  NVIC_PRI7_R = (NVIC_PRI7_R&0xFF1FFFFF)|0x00400000; // (g) bits:23-21 for PORTF, set priority to 5
+  NVIC_EN0_R |= 0x40000000;      // (h) enable interrupt 30 in NVIC
+}
