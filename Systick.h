1755230a3caac121946404fe870f9441f00f2e61
KyleWyckoff <105688322+KyleWyckoff@users.noreply.github.com>
2025-04-09 22:54:03 -0700

    Create Systick.h

---
+++ Systick.h
@@ -0,0 +1,22 @@
+// Systick.h
+// Runs on TM4C123
+// CECS 347 Project 2 - Space Invaders
+// Group number:
+// Group members:
+
+
+
+// SysTick.h
+// Runs on TM4C123
+// By Dr. Min He
+// December 10th, 2018
+ 
+#include <stdint.h>
+
+// Time delay using busy wait.
+void SysTick_Wait1us(uint8_t delay);
+void SysTick_Init(void);
+void SysTick_Stop(void);
+uint32_t SysTick_Get_MC_Elapsed(void);
+
+
