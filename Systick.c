51a671a5a064bde399e79b32c90d28abd415d4bc
Bronson Garel <78556724+bronsongarel@users.noreply.github.com>
2025-04-14 14:33:01 -0700

    Update Systick.c

--- Systick.c
+++ Systick.c
@@ -1,28 +1,20 @@
 // Systick.c
 // Runs on TM4C123
-// CECS 347 Project 2 - Space Invaders
-// Group number:
-// Group members:
-// By Min He
-// November 15, 2022
-#include "Systick.h"
-
-
-// SysTick.c
-// Runs on TM4C123
-// By Dr. Min He
-// December 10th, 2018
+// CECS 347 Project 3 - Space Invaders
+// Group number: 9
+// Group members: Jose Ambriz, Bronson Garel, Jonathan Kim, Kyle Wyckoff
 
 #include <stdint.h>
 #include "SysTick.h"
 #include "tm4c123gh6pm.h"
 
-#define ONE_MICRO_SECOND          16     // number of machine cycles to generate 1us delay for 16MHz system clock
+#define ONE_MICRO_SECOND          16     	// number of machine cycles to generate 1us delay for 16MHz system clock
+#define POINT_ONE_SECOND					8000000 // number of machine cycles to generate 0.1 delay for 50MHz system clock
 void SysTick_Init(void){
   NVIC_ST_CTRL_R = 0;
   NVIC_ST_RELOAD_R = NVIC_ST_RELOAD_M; // number of counts to wait
-  NVIC_ST_CURRENT_R = 0; // any value written to CURRENT clears   
-	NVIC_ST_CTRL_R |= NVIC_ST_CTRL_ENABLE+NVIC_ST_CTRL_CLK_SRC; // enable SysTick with core clock
+	NVIC_SYS_PRI3_R = (NVIC_SYS_PRI3_R&0x00FFFFFF)|0x40000000;
+	NVIC_ST_CTRL_R |= NVIC_ST_CTRL_CLK_SRC | NVIC_ST_CTRL_INTEN;
 }
 
 // Disable Systick timer
@@ -35,48 +27,24 @@ uint32_t SysTick_Get_MC_Elapsed(void){
 	return NVIC_ST_RELOAD_R-NVIC_ST_CURRENT_R;
 }
 
-// Time delay using busy wait.
-// This function assumes 16 MHz system clock.
-void SysTick_Wait1us(uint8_t delay){
-  NVIC_ST_CTRL_R = 0;
-  NVIC_ST_RELOAD_R = delay*ONE_MICRO_SECOND-1; // number of counts to wait
-  NVIC_ST_CURRENT_R = 0; // any value written to CURRENT clears
-	NVIC_ST_CTRL_R |= NVIC_ST_CTRL_ENABLE+NVIC_ST_CTRL_CLK_SRC;
-  while((NVIC_ST_CTRL_R&NVIC_ST_CTRL_COUNT)==0); // wait for count flag
-  NVIC_ST_CTRL_R = 0;
+void SysTick_Wait_0_1sec(void){
+	NVIC_ST_RELOAD_R = POINT_ONE_SECOND-1; // number of counts to wait
+	NVIC_ST_CURRENT_R = 0; // any value written to CURRENT clears
+	NVIC_ST_CTRL_R |= NVIC_ST_CTRL_ENABLE;
+	//while((NVIC_ST_CTRL_R&NVIC_ST_CTRL_COUNT)==0); // wait for count flag
+	//NVIC_ST_CTRL_R = 0;
 }
 
-
-void SysTick_Wait(uint32_t delay){
-
-	NVIC_ST_RELOAD_R = delay - 1;
-  NVIC_ST_CURRENT_R = 0;                // any write to current clears it
-	NVIC_ST_CTRL_R |= NVIC_ST_CTRL_ENABLE;
-  
-  while(!(NVIC_ST_CTRL_R & NVIC_ST_CTRL_COUNT)){};
-	
+void SysTick_BusyWait_0_1sec(uint8_t delay){
+	NVIC_ST_CTRL_R &= ~NVIC_ST_CTRL_INTEN;
+	while(delay > 0){
 		NVIC_ST_CTRL_R &= ~NVIC_ST_CTRL_ENABLE;
-		
-	}
-
-	
-	
-void SysTick_Wait_0_1sec(void){
-	uint8_t count = 0;
-	while(count != 200){
-		if(count <= 192){
-			NVIC_ST_RELOAD_R = 27160;
-		}
-		else{
-			NVIC_ST_RELOAD_R = 65535;
-		}
-		NVIC_ST_CURRENT_R = 0;                // any write to current clears it
+		NVIC_ST_RELOAD_R = POINT_ONE_SECOND-1; // number of counts to wait
+		NVIC_ST_CURRENT_R = 0; // any value written to CURRENT clears
 		NVIC_ST_CTRL_R |= NVIC_ST_CTRL_ENABLE;
-		
-		while(!(NVIC_ST_CTRL_R & NVIC_ST_CTRL_COUNT)){};
-		
-			NVIC_ST_CTRL_R &= ~NVIC_ST_CTRL_ENABLE;
-		
-		count++;
-	}	
+		while((NVIC_ST_CTRL_R&NVIC_ST_CTRL_COUNT)==0); // wait for count flag
+		NVIC_ST_CTRL_R &= ~NVIC_ST_CTRL_ENABLE;
+		delay--;
+	}
+	NVIC_ST_CTRL_R |= NVIC_ST_CTRL_INTEN;
 }
