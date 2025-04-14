9c968876abee5858460d748cdd2735cdc5ccf2b8
Bronson Garel <78556724+bronsongarel@users.noreply.github.com>
2025-04-14 14:33:50 -0700

    Update switch.h

--- switch.h
+++ switch.h
@@ -1,7 +1,8 @@
 // switches.h
-// Runs on TM4C123, starter file for space invader game
-// Min He
-// November 15, 2022
+// Runs on TM4C123
+// CECS 347 Project 3 - Space Invaders
+// Group number: 9
+// Group members: Jose Ambriz, Bronson Garel, Jonathan Kim, Kyle Wyckoff
 
 // Initialize the onboard two switches.
 void Switch_Init(void);
