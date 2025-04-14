887f75c7a560b819f90127ff153223f9838c8407
Bronson Garel <78556724+bronsongarel@users.noreply.github.com>
2025-04-14 14:33:19 -0700

    Update Systick.h

--- Systick.h
+++ Systick.h
@@ -1,16 +1,9 @@
 // Systick.h
 // Runs on TM4C123
-// CECS 347 Project 2 - Space Invaders
-// Group number:
-// Group members:
+// CECS 347 Project 3 - Space Invaders
+// Group number: 9
+// Group members: Jose Ambriz, Bronson Garel, Jonathan Kim, Kyle Wyckoff
 
-
-
-// SysTick.h
-// Runs on TM4C123
-// By Dr. Min He
-// December 10th, 2018
- 
 #include <stdint.h>
 
 // Time delay using busy wait.
@@ -18,5 +11,5 @@ void SysTick_Wait1us(uint8_t delay);
 void SysTick_Init(void);
 void SysTick_Stop(void);
 uint32_t SysTick_Get_MC_Elapsed(void);
-void SysTick_Wait(uint32_t delay);
 void SysTick_Wait_0_1sec(void);
+void SysTick_BusyWait_0_1sec(uint8_t delay);
